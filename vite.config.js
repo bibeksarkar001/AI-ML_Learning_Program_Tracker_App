bc1c3031547ab2317138298acd2f625384ab6fab
bibeksarkar001 <bibeksarkar1397@gmail.com>
2026-04-29 01:34:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mastery-roadmap/', // Replace with your repo name
+  // This MUST match your repository name exactly
+  base: '/AI-ML_Learning_Program_Tracker_App/', 
 })
