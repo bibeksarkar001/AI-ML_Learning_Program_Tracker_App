6d9c98e680adabcbb37778713839ed0f04d05327
bibeksarkar001 <bibeksarkar1397@gmail.com>
2026-04-29 00:38:05 +0530

    vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/mastery-roadmap/', // Replace with your repo name
+})
